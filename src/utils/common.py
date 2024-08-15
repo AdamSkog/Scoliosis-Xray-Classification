e8bc3025c0ff6df416469f6f3891d6f751087187
AdamSkog <adamskoglund2022@gmail.com>
2024-08-14 20:19:22 -0700

    Basic model training in notebook

---
+++ src/utils/common.py
@@ -0,0 +1,85 @@
+import json
+import logging
+import os
+from pathlib import Path
+
+import joblib
+import yaml
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from ensure import ensure_annotations
+
+logger = logging.getLogger(__name__)
+logger.basicConfig(level=logging.INFO)
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    Reads a YAML file and returns its contents as a ConfigBox object.
+
+    Args:
+        path_to_yaml (Path): Path to the YAML file.
+
+    Raises:
+        ValueError: If the YAML file is empty.
+        Exception: For any other exceptions that occur while reading the file.
+
+    Returns:
+        ConfigBox: The contents of the YAML file as a ConfigBox object.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Creates a list of directories.
+
+    Args:
+        path_to_directories (list): List of paths for the directories to be created.
+        verbose (bool, optional): If True, logs the creation of directories. Defaults to True.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Directory created at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """
+    Saves a dictionary as a JSON file.
+
+    Args:
+        path (Path): Path to the JSON file.
+        data (dict): Data to be saved in the JSON file.
+    """
+    with open(path, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        logger.info(f"JSON file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Loads data from a JSON file and returns it as a ConfigBox object.
+
+    Args:
+        path (Path): Path to the JSON file.
+
+    Returns:
+        ConfigBox: The contents of the JSON file as a ConfigBox object.
+    """
+    with open(path, "r") as json_file:
+        content = json.load(json_file)
+        logger.info(f"JSON file loaded from: {path}")
+        return ConfigBox(content)
